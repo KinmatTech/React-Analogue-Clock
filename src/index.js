b8f67a3903a7c687aa4946b50b1500eb24f36acb
Okechukwu Ani <mathew.ani@unn.edu.ng>
2022-10-26 00:35:43 +0100

    https://github.com/KinmatTech/React-Analogue-Clock.git

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.css";
 import Clock from "./Clock";
 
 function App() {
   return (
     <div className="App">
-      <h1>Analog Clock React App </h1>
+      <h1>Analog Clock App </h1>
       <h3>Author: Kinmat Tech.</h3>
       <Clock />
     </div>
