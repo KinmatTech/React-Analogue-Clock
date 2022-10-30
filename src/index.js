91b3eae101d7d3a0ee2b3ffbc4f78b1db639c16c
Okechukwu Ani <mathew.ani@unn.edu.ng>
2022-10-30 02:21:46 +0100

    https://github.com/KinmatTech/React-Analogue-Clock.git

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h2><font color="#009800">Clock App</font> </h2>
-      <br/> <br/> <br/> <br/> <br/> <br/>
+      <br/> <br/> <br/> 
       <h3> 
         <font color="black">Kinmat Tech.</font></h3>
       <Clock />
